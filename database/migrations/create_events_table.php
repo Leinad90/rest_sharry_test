f5fa35f87eab4d7bbd00dd0537b21933adbd5475
daniel <daniel@daniel-pc>
2021-03-13 20:59:20 +0100

    Tables

---
+++ database/migrations/create_events_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('content');
+            $table->date('valid_from')->nullable();
+            $table->date('valid_to')->nullable();
+            $table->decimal('gps_lat');
+            $table->decimal('gps_lng');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
